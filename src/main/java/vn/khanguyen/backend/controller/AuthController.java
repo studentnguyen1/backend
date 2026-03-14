af6c9366a1b51420926060120dde8864cfc1bb33
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-03-15 00:13:44 +0700

    Add JWT

--- src/main/java/vn/khanguyen/backend/controller/AuthController.java
+++ src/main/java/vn/khanguyen/backend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package vn.khanguyen.backend.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.khanguyen.backend.domain.dto.LoginDTO;
+import vn.khanguyen.backend.domain.dto.RestLoginDTO;
 import vn.khanguyen.backend.util.SecurityUtil;
 
 @RestController
@@ -23,7 +25,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<RestLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDTO.getUsername(), loginDTO.getPassword());
@@ -31,10 +33,12 @@ public class AuthController {
         // xác thực người dùng => cần viết hàm loadUserByUsername // xac thuc password
         // va truyen username, passw
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        // tao token
+        String access_token = this.securityUtil.createToken(authentication);
+        RestLoginDTO restLoginDTO = new RestLoginDTO();
+        restLoginDTO.setAccessToken(access_token);
 
-        this.securityUtil.createToken(authentication);
-
-        return ResponseEntity.ok().body(loginDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(restLoginDTO);
 
     }
 
--- src/main/java/vn/khanguyen/backend/util/SecurityUtil.java
+++ src/main/java/vn/khanguyen/backend/util/SecurityUtil.java
@@ -1,19 +1,46 @@
 package vn.khanguyen.backend.util;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityUtil {
+    private final JwtEncoder jwtEncoder;
+
+    public SecurityUtil(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
 
     public static final MacAlgorithm JWT_ALGORITHM = MacAlgorithm.HS512;
 
     @Value("${jobhunter.jwt.token-validity-in-seconds}")
     private long jwtExpiration;
 
-    public void createToken(Authentication authentication) {
+    public String createToken(Authentication authentication) {
+        // tao thoi gian ke tu luc hien tai + thoi gian ton tai cua token
+        Instant now = Instant.now();
+        Instant validity = now.plus(this.jwtExpiration, ChronoUnit.SECONDS);
+
+        // tao payload
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(validity)
+                .subject(authentication.getName()) // name
+                .claim("nguyen", authentication) // data
+                .build();
+        // tao header
+        JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
+
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
 
     }
 
