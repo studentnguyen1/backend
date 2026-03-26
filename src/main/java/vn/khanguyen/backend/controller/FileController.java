3dbd0d4e83b7be4239c8121ac5a35639e6e90d36
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-03-26 09:02:57 +0700

    Add handle exception of file

--- src/main/java/vn/khanguyen/backend/controller/FileController.java
+++ src/main/java/vn/khanguyen/backend/controller/FileController.java
@@ -3,6 +3,8 @@ package vn.khanguyen.backend.controller;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import vn.khanguyen.backend.domain.res.file.ResUploadFileDTO;
 import vn.khanguyen.backend.service.FileService;
 import vn.khanguyen.backend.util.annotation.ApiMessage;
+import vn.khanguyen.backend.util.error.ResourceNotFoundException;
 
 @RestController
 public class FileController {
@@ -28,18 +31,29 @@ public class FileController {
 
     @PostMapping("/files")
     @ApiMessage("Upload single file")
-    public ResponseEntity<ResUploadFileDTO> uploadFile(@RequestParam("file") MultipartFile file,
-            @RequestParam("folder") String folder) throws URISyntaxException, IOException {
-
-        // check folder exist
+    public ResponseEntity<ResUploadFileDTO> uploadFile(
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            @RequestParam("folder") String folder) throws URISyntaxException, IOException, ResourceNotFoundException {
+        // validate file
+        if (file == null || file.isEmpty()) {
+            throw new ResourceNotFoundException("File is empty. Please upload a file");
+        }
+        // check type of file
+        String fileName = file.getOriginalFilename();
+        List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc", "docx");
+        boolean isValid = allowedExtensions.stream().anyMatch(item -> fileName.toLowerCase().endsWith(item));
+        if (!isValid) {
+            throw new ResourceNotFoundException("Invalid file extension only allow: " +
+                    allowedExtensions.toString());
+        }
 
         // tao folder
         this.fileService.createDirectory(baseURI + folder);
 
         // upload file
-        String fileName = this.fileService.store(file, folder);
+        String fileUpload = this.fileService.store(file, folder);
 
-        ResUploadFileDTO res = new ResUploadFileDTO(fileName, Instant.now());
+        ResUploadFileDTO res = new ResUploadFileDTO(fileUpload, Instant.now());
 
         return ResponseEntity.ok().body(res);
 
